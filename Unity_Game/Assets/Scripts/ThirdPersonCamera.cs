b5effe5e9e9c42651fd4b30223449b0191f4e8ab
AmjadMohamed <amjadmohamed55@gmail.com>
2019-09-19 22:28:05 +0200

    First commit - char movement
    Added character movement

---
+++ Unity_Game/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public bool LockCursor;
+    public float MouseSensitivity = 10;
+    public Transform player;
+    public float DistanceFromPlayer = 2;
+   // public Vector2 PitchMinMax = new Vector2(-40, 85);
+
+    public float RotationSmoothTime = .12f;
+    Vector3 RotationSmoothVelocity;
+    Vector3 CurrentRotation;
+
+   // float yaw;
+  //  float pitch;
+
+    void Start()
+    {
+        if(LockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        //yaw += Input.GetAxis("Mouse X") * MouseSensitivity;
+       // pitch -= Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+       // pitch = Mathf.Clamp(pitch, PitchMinMax.x, PitchMinMax.y);
+
+      //  CurrentRotation = Vector3.SmoothDamp(CurrentRotation, new Vector3(pitch, yaw), ref RotationSmoothVelocity, RotationSmoothTime);
+       // transform.eulerAngles = CurrentRotation;
+
+        transform.position = player.position - transform.forward * DistanceFromPlayer;
+    }
+
+
+
+}
---
+++ Unity_Game/Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    public float WalkSpeed = 2;
+    public float RunSpeed = 4;
+
+    public float TurnSmoothlyTime = .2f;
+    float TurnSmoothlyVelocity;
+
+    public float SpeedSmoothlyTime = .1f;
+    float SpeedSmoothlyVelocity;
+    float CurrentSpeed;
+
+    Animator anim;
+    public Transform cam;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+       // cam = Camera.main.transform;
+    }
+
+    
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 InputDirection = input.normalized;
+
+        //بحسب الزاوية اللي باصص ليها عشان اتحرك ناحيتها 
+        if(InputDirection != Vector2.zero)
+        {
+            float TargetRotation = Mathf.Atan2(InputDirection.x, InputDirection.y) * Mathf.Rad2Deg + cam.eulerAngles.y; 
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetRotation, ref TurnSmoothlyVelocity, TurnSmoothlyTime);
+        }
+
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+        if(running)
+        {
+            float targetSpeed = RunSpeed * InputDirection.magnitude;
+            CurrentSpeed = Mathf.SmoothDamp(CurrentSpeed, targetSpeed, ref SpeedSmoothlyVelocity, SpeedSmoothlyTime);
+            transform.Translate(transform.forward * CurrentSpeed * Time.deltaTime, Space.World);
+            float AnimSpeed = 1 * InputDirection.magnitude;
+            anim.SetFloat("Blend", AnimSpeed , SpeedSmoothlyTime , Time.deltaTime);
+
+        }
+        else
+        {
+            float targetSpeed = WalkSpeed * InputDirection.magnitude;
+            CurrentSpeed = Mathf.SmoothDamp(CurrentSpeed, targetSpeed, ref SpeedSmoothlyVelocity, SpeedSmoothlyTime);
+            transform.Translate(transform.forward * CurrentSpeed * Time.deltaTime, Space.World);
+            float AnimSpeed = .5f * InputDirection.magnitude;
+            anim.SetFloat("Blend", AnimSpeed, SpeedSmoothlyTime, Time.deltaTime);
+        }
+
+    }
+}
